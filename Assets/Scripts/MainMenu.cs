1af6f53f5dba44067c74d90a9e5b465d4f30f66c
2615283 <132197652+2615283@users.noreply.github.com>
2023-09-20 22:28:48 +0200

    Some Menu stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void playGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(1);
     }
 
     public void Quit()

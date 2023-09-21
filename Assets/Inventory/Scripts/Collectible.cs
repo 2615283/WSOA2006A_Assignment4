d50ff2df923613e6bb565ae90246a18a8291f149
2615283 <132197652+2615283@users.noreply.github.com>
2023-09-21 07:46:56 +0200

    Revert "Stuff"
    This reverts commit 323895a3e8aac03d04ae85260c8bd5301f146867.

--- Assets/Inventory/Scripts/Collectible.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Collectible : MonoBehaviour
-{
-   // gives the item a name or description
-    public string ItemType;
-}
